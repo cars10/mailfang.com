d01d74ea2ecd9e50af31a2d5faec1262a484841c
Carsten König <carstenkoenig92@gmail.com>
2026-02-16 19:54:10 +0100

    astro font feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Roboto',
+        cssVariable: '--font-roboto',
+        weights: ['100 900'],
+        styles: ['normal', 'italic'],
+        subsets: ['latin', 'latin-ext'],
+        fallbacks: ['sans-serif'],
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
