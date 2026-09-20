c7ecaa9228091d02578624e40abdf4b40f1e3aff
Carsten König <carstenkoenig92@gmail.com>
2026-09-20 15:36:12 +0200

    astro v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mailfang.com',
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: 'Roboto',
-        cssVariable: '--font-roboto',
-        weights: [300, 400, 500, 700],
-        styles: ['normal'],
-        subsets: ['latin'],
-        fallbacks: ['sans-serif'],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Roboto',
+      cssVariable: '--font-roboto',
+      weights: [300, 400, 500, 700],
+      styles: ['normal'],
+      subsets: ['latin'],
+      fallbacks: ['sans-serif'],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
